d3be50da5e9e0297115c6cb9fefc07554a6bc370
mohit1453 <67677509+mohit1453@users.noreply.github.com>
2022-02-15 15:59:43 +0530

    Create mousein-out.js

---
+++ mousehover/mousein-out.js
@@ -0,0 +1,9 @@
+var outerDiv=document.getElementById("outer")
+outer.addEventListener("mouseover",function()
+{
+    console.log("Mouse over bro");
+})
+outer.addEventListener("mouseout",function()
+{
+    console.log("Mouse out bro");
+})
